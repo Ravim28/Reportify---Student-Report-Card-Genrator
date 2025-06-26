f89abaa1805ac66db8b21d55243d4594d45a5b12
Ravim28 <ayushmalviya990@gmail.com>
2025-06-26 10:35:25 +0530

    vite update

--- Frontend/Report-card/vite.config.js
+++ Frontend/Report-card/vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'https://reportify-student-report-card-genrator-1.onrender.com', // Proxy API calls to backend
+    },
+  },
+  
+});
